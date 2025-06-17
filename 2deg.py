a02766cb1f16268f2f442999f90a13435e840518
Yangyukai <beir6108@gmail.com>
2025-06-17 09:38:40 +0800

    first commit

--- 2deg.py
+++ 2deg.py
@@ -10,7 +10,6 @@ temp = temp.assign_coords(valid_time=pd.to_datetime(temp.valid_time.values))
 # 2. 粗化到 5°(用 mean)
 temp_coarse = temp.coarsen(latitude=8, longitude=8, boundary='trim').mean()
 
-
 # 3. 儲存
 encoding = {'t': {'dtype': 'int8', 'zlib': True, 'complevel': 5}}
 heatwave_events.to_netcdf('./data/heatwave_events_2deg(1980-2002).nc', encoding=encoding)
